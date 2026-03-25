1284aebb4dcffa454b0902f757547e2a166bc28c
diana-hav <dgavrish1205@gmail.com>
2026-03-25 15:02:10 +0200

    fix buy button in main banner

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,7 +1,23 @@
+import { useLocation } from "react-router-dom";
 import backpack from "@/assets/images/backpack.png";
 import "./Banner.css";
 
 const Banner = () => {
+  const location = useLocation();
+
+  const handleBuyNow = () => {
+    const productsSection = document.getElementById("products");
+    if (productsSection) {
+      productsSection.scrollIntoView({ behavior: "smooth" });
+      return;
+    }
+
+    // Якщо секція тимчасово відсутня (з іншого маршруту), направляємо на головну з хешем
+    if (location.pathname !== "/") {
+      window.location.replace("/#products");
+    }
+  };
+
   return (
     <div className="sub-container">
       <div className="banner">
@@ -10,9 +26,9 @@ const Banner = () => {
             Скидка до 50% на <br></br>выбранные товары
           </h1>
           <span className="is-buy-now">
-            <a href="#products">
-              <button className="btn-rounded buy-now">Купить</button>
-            </a>
+            <button className="btn-rounded buy-now" onClick={handleBuyNow}>
+              Купить
+            </button>
           </span>
         </div>
         <div className="subject">
